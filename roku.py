dabcf24722ec93312de37c144a9b49f626397d95
Michael Provenzano <50926811+MikeyPPPPPPPP@users.noreply.github.com>
2021-06-30 16:15:17 -0700

    Update roku.py

--- roku.py
+++ roku.py
@@ -1,5 +1,3 @@
-
-
 import requests
 class roku:
 	def __init__(self, ip):
@@ -7,49 +5,30 @@ class roku:
 		self.url = ''
 		self.headers = {'User-Agent': "RoByte/2.1.8.2 CFNetwork/1220.1 Darwin/20.3.0"}
 
-	def _build_url(self,text):
+	def build_url(self,text):
 		self.url = 'http://'+self.ip+':8060/keypress/'+text
 
 	def power(self):
-		build_url('Power')
+		self.build_url('Power')
 		response = requests.post(self.url, headers=self.headers)
 
 	def rev(self):
-		build_url('Rev')
+		self.build_url('Rev')
 		response = requests.post(self.url, headers=self.headers)
 	def fwd(self):
-		build_url('Fwd')
+		self.build_url('Fwd')
 		response = requests.post(self.url, headers=self.headers)
 
 	def info(self):
-		build_url('Info')
+		self.build_url('Info')
 		response = requests.post(self.url, headers=self.headers)
 	def replay(self):
-		build_url('InstantReplay')
+		self.build_url('InstantReplay')
 		response = requests.post(self.url, headers=self.headers)
 	def home(self):
-		build_url('Home')
+		self.build_url('Home')
 		response = requests.post(self.url, headers=self.headers)
 
-ppak = roku("192.168.86.41")
-ppak.power()
-'''	
-power = 'http://192.168.86.41:8060/keypress/Power'
+#ppak = roku("192.168.86.41")
+#ppak.power()
 
-
-Info
-Fwd
-Rev
-Play
-Back
-down
-up
-left
-right
-InstantReplay
-Home
-
-Select
-
-user-agent = "RoByte/2.1.8.2 CFNetwork/1220.1 Darwin/20.3.0"
-'''
