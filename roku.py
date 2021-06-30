992f11da918f4c177aa38ddf13d5843521a145d0
Michael Provenzano <50926811+MikeyPPPPPPPP@users.noreply.github.com>
2021-06-30 13:27:14 -0700

    Update roku.py

--- roku.py
+++ roku.py
@@ -1,41 +1,38 @@
 
+
 import requests
 class roku:
 	def __init__(self, ip):
 		self.ip = ip
 		self.url = ''
+		self.headers = {'User-Agent': "RoByte/2.1.8.2 CFNetwork/1220.1 Darwin/20.3.0"}
+
 	def _build_url(self,text):
 		self.url = 'http://'+self.ip+':8060/keypress/'+text
 
 	def power(self):
 		build_url('Power')
-		headers = {'User-Agent': "RoByte/2.1.8.2 CFNetwork/1220.1 Darwin/20.3.0"}
-		response = requests.post(self.url, headers=headers)
+		response = requests.post(self.url, headers=self.headers)
 
 	def rev(self):
 		build_url('Rev')
-		headers = {'User-Agent': "RoByte/2.1.8.2 CFNetwork/1220.1 Darwin/20.3.0"}
-		response = requests.post(self.url, headers=headers)
+		response = requests.post(self.url, headers=self.headers)
 	def fwd(self):
 		build_url('Fwd')
-		headers = {'User-Agent': "RoByte/2.1.8.2 CFNetwork/1220.1 Darwin/20.3.0"}
-		response = requests.post(self.url, headers=headers)
+		response = requests.post(self.url, headers=self.headers)
 
 	def info(self):
 		build_url('Info')
-		headers = {'User-Agent': "RoByte/2.1.8.2 CFNetwork/1220.1 Darwin/20.3.0"}
-		response = requests.post(self.url, headers=headers)
+		response = requests.post(self.url, headers=self.headers)
 	def replay(self):
 		build_url('InstantReplay')
-		headers = {'User-Agent': "RoByte/2.1.8.2 CFNetwork/1220.1 Darwin/20.3.0"}
-		response = requests.post(self.url, headers=headers)
+		response = requests.post(self.url, headers=self.headers)
 	def home(self):
 		build_url('Home')
-		headers = {'User-Agent': "RoByte/2.1.8.2 CFNetwork/1220.1 Darwin/20.3.0"}
-		response = requests.post(self.url, headers=headers)
+		response = requests.post(self.url, headers=self.headers)
 
-#ppak = roku("192.168.86.41")
-#ppak.prank()
+ppak = roku("192.168.86.41")
+ppak.power()
 '''	
 power = 'http://192.168.86.41:8060/keypress/Power'
 
